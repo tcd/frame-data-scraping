572b4540cb672b585878eba88728839c0e982f78
Clay Dunston <claydunston@gmail.com>
2021-09-17 21:40:17 -0500

    Add try/catch blocks

--- src/srk/main.ts
+++ src/srk/main.ts
@@ -7,64 +7,49 @@ import { characters } from "@data"
 import { parseOldSRKFrameData } from "."
 
 export async function parse3SFrameData(trialNumber: string) {
-    const rootDir = await pkgDir(__dirname)
+    try {
+        const rootDir = await pkgDir(__dirname)
 
-    for (const character of characters) {
-        const outFile     = `${rootDir}/out/tsv/${trialNumber}/${character}.tsv`
-        const oldFile     = `${rootDir}/out/html/srk/old/${character}.html`
-        const oldFileData = fs.readFileSync(oldFile, "utf8")
-        await parseOldSRKFrameData(character, oldFileData)
-            .then(async (moves) => {
-                let data = moves.map(x => x.toCombo())
-                await writeJsonToTsv(data, outFile)
-            })
-            .catch(err => console.log(err))
-    }
-}
-
-export async function collectKeys() {
-    const rootDir      = await pkgDir(__dirname)
-    const allFrameData = []
-
-    for (const character of characters) {
-        const oldFile     = `${rootDir}/out/html/srk/old/${character}.html`
-        const oldFileData = fs.readFileSync(oldFile, "utf8")
-        await parseOldSRKFrameData(character, oldFileData)
-            .then(async (data) => {
-                allFrameData.push(data)
-                console.log(`${character} complete`)
-            })
-            .catch(err => console.log(err))
+        for (const character of characters) {
+            const outFile     = `${rootDir}/out/tsv/${trialNumber}/${character}.tsv`
+            const oldFile     = `${rootDir}/out/html/srk/old/${character}.html`
+            const oldFileData = fs.readFileSync(oldFile, "utf8")
+            await parseOldSRKFrameData(character, oldFileData)
+                .then(async (moves) => {
+                    let data = moves.map(x => x.toCombo())
+                    await writeJsonToTsv(data, outFile)
+                })
+                .catch(err => console.log(err))
+        }
+    } catch (error) {
+        console.log(error)
     }
 
-    const keys = []
-    allFrameData.flat(1).map(x => keys.push(...Object.keys(x)))
-    const data = JSON.stringify(uniq(keys), null, 2)
-
-    const filePath = `${rootDir}/out/json/3s.json`
-    fs.writeFileSync(filePath, data)
-    console.log(`File written: '${filePath}'`)
 }
 
 export async function collectAllDataTSV() {
-    const rootDir      = await pkgDir(__dirname)
-    const outFile      = `${rootDir}/out/tsv/all-moves-v2.tsv`
-    const allFrameData = []
+    try {
+        const rootDir      = await pkgDir(__dirname)
+        const outFile      = `${rootDir}/out/tsv/all-moves-v2.tsv`
+        const allFrameData = []
 
-    for (const character of characters) {
-        const oldFile     = `${rootDir}/out/html/srk/old/${character}.html`
-        const oldFileData = fs.readFileSync(oldFile, "utf8")
-        await parseOldSRKFrameData(character, oldFileData)
-            .then(async (data) => {
-                allFrameData.push(data)
-                console.log(`${character} complete`)
-            })
-            .catch(err => console.log(err))
+        for (const character of characters) {
+            const oldFile     = `${rootDir}/out/html/srk/old/${character}.html`
+            const oldFileData = fs.readFileSync(oldFile, "utf8")
+            await parseOldSRKFrameData(character, oldFileData)
+                .then(async (data) => {
+                    allFrameData.push(data)
+                    console.log(`${character} complete`)
+                })
+                .catch(err => console.log(err))
+        }
+
+        let outData = allFrameData.flat(1).map(x => flattenObject(x.toCombo().slimData()))
+
+        await writeJsonToTsv(outData, outFile)
+    } catch (error) {
+        console.log(error)
     }
-
-    let outData = allFrameData.flat(1).map(x => flattenObject(x.toCombo()))
-
-    await writeJsonToTsv(outData, outFile)
 }
 
 export async function collectAllDataJSON() {
