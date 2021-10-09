134502db7d91d644091920dff26e4b9d9319760e
Clay Dunston <claydunston@gmail.com>
2021-10-08 19:59:23 -0500

    Add data from FAT 3

---
+++ src/@types/FAT/ThirdStrikeFrameData.ts
@@ -0,0 +1,24 @@
+export interface ThirdStrikeFrameData {
+  moveName:      string;
+  plnCmd:        string;
+  numCmd:        string;
+  startup:       number;
+  active:        number;
+  recovery:      number;
+  onHit:         number;
+  onBlock:       number;
+  onHitCrouch:   number;
+  moveType:      string;
+  cancelsTo:     string[];
+  parry:         string;
+  karaRange:     number;
+  damage:        number;
+  stun:          number;
+  meterAtkWhiff: number;
+  meterAtkHit:   number;
+  meterAtkBlk:   number;
+  meterOppHit:   number;
+  meterOppBlk:   number;
+  moveMotion:    string;
+  moveButton:    string;
+}
