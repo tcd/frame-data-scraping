5ef7aeee8d9300f302234540ecf8b2dc18e7fa1e
Clay Dunston <claydunston@gmail.com>
2021-10-17 14:10:30 -0500

    Well that's great

--- src/@types/FAT/ThirdStrikeToComboNotebook.ts
+++ src/@types/FAT/ThirdStrikeToComboNotebook.ts
@@ -1,28 +1,30 @@
-import { Combo, IThirdStrikeFrameData } from "@types"
+import { Combo, IThirdStrikeFrameData, COMBO_TYPES } from "@types"
 
 export const ThirdStrikeToComboNotebook = (fatData: IThirdStrikeFrameData): Combo => {
     let combo = new Combo()
+
+    combo.content = fatData?.numCmd
+
+    combo.frame_data_v2.startup.numeric_value = fatData?.startup
+    combo.frame_data_v2.active.numeric_value  = fatData?.active
+    combo.frame_data_v2.recovery.numeric_value  = fatData?.recovery
+    combo.frame_data_v2.hit_advantage.numeric_value  = fatData?.onHit
+    // combo.frame_data_v2.crouching_hit_advantage.numeric_value  = fatData?.onHitCrouch
+
     return combo
 }
 
-const moveTypes = [
-    "normal",
-    "throw",
-    "universal-oh",
-    "taunt",
-    "special",
-    "movement-special",
-    "super",
-    "command-grab",
-]
+const moveTypeToComboType = (moveType: string): string => {
+    return (moveTypesMap[moveType] || null)
+}
 
 const moveTypesMap = {
-    "normal": "",
-    "throw": "",
-    "universal-oh": "",
-    "taunt": "",
-    "special": "",
-    "movement-special": "",
-    "super": "",
-    "command-grab": "",
+    "normal":           COMBO_TYPES.NORMAL,
+    "special":          COMBO_TYPES.SPECIAL,
+    "movement-special": COMBO_TYPES.SPECIAL,
+    "command-grab":     COMBO_TYPES.SPECIAL,
+    "super":            COMBO_TYPES.SUPER,
+    "throw":            COMBO_TYPES.UNIVERSAL_MECHANIC,
+    "universal-oh":     COMBO_TYPES.UNIVERSAL_MECHANIC,
+    "taunt":            COMBO_TYPES.UNIVERSAL_MECHANIC,
 }
